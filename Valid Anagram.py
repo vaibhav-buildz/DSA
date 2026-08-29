653053020e47f1cdbb2dadf6de0372c79d09acb9
vaibhav-buildz <vansh9793693385@gmail.com>
2026-08-30 02:04:25 +0530

    feat: add implementations for Maximum Subarray and Product of Array Except Self problems

---
+++ Valid Anagram.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        count = [0] * 26
+        for a, b in zip(s, t):
+            count[ord(a) - 97] += 1
+            count[ord(b) - 97] -= 1
+
+        return not any(count)
+    
