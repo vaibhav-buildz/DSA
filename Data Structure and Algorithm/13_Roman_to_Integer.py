960d4bffa5b78709e4fbf77f73e3529e7267d162
vaibhav-buildz <vansh9793693385@gmail.com>
2026-09-01 23:26:11 +0530

    feat: add newline at end of file and improve formatting in Roman to Integer solution

--- Data Structure and Algorithm/13_Roman_to_Integer.py
+++ Data Structure and Algorithm/13_Roman_to_Integer.py
@@ -1,3 +1,4 @@
+#1st Method 
 class Solution:
     def romanToInt(self, s: str) -> int:
         translations = {
@@ -21,3 +22,18 @@ class Solution:
         for char in s:
             num += translations[char]
         return num
+
+
+
+
+#2nd Method
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        values = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+        total = 0
+        for i in range(len(s)):
+            if i + 1 < len(s) and values[s[i]] < values[s[i+1]]:
+                total -= values[s[i]]
+            else:
+                total += values[s[i]]
+        return total
